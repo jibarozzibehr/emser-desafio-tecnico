8e163ad255f803ceb63fadcdedd6efa5875a7fd4
jibarozzibehr <jibarozzibehr@gmail.com>
2022-11-26 06:11:17 -0300

    Initial commit

---
+++ backend/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Proyecto(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_length=64, unique=True)
+    activo = models.BooleanField()
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=64, unique=True)
+    password = models.CharField(max_length=64)
+    nombre = models.CharField(max_length=64)
+    apellido = models.CharField(max_length=64)
+    telefono = models.BigIntegerField()
+    email = models.EmailField(max_length=256)
+    tipo = models.IntegerField()
+    activo = models.BooleanField()
+
+class ProyectoUsuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario_id = models.ForeignKey(to=Usuario, on_delete=models.CASCADE)
+    proyecto_id = models.ForeignKey(to=Proyecto, on_delete=models.CASCADE)
+    activo = models.BooleanField()
